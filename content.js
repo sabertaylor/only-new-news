c797670b7a5bfda7cfb66d3026ec66a20f701690
Saber Taylor <saber.taylor@gmail.com>
2020-03-15 16:55:47 -0700

    Added reddit.

--- content.js
+++ content.js
@@ -3,43 +3,91 @@ if (window.location.href === "https://news.ycombinator.com/") {
 
   console.log("Did storage persist?");
 
-  chrome.storage.local.get(['news'], function(result) {
-      if (!result.news) {
-        result.news = {};
+  chrome.storage.local.get(['hnNews'], function(result) {
+      if (!result.hnNews) {
+        result.hnNews = {};
       }
 
-      console.log('Read ' + Object.keys(result.news).length + ' news identifiers from storage.');
+      console.log('Read ' + Object.keys(result.hnNews).length + ' news identifiers from storage.');
 
-      console.log("Loading current page's news identifiers.");
       let things = $('.athing')
       let now = Date.now();
       let news = {};
       things.each(function() { news[$(this).attr('id')] = now; });
+      console.log("Found " + Object.keys(news).length + " news identifiers in current page.");
 
-      console.log('Hiding stories that have been seen before.');
-
-      for(let i = 0; i <= Object.keys(result.news).length - 1; i++) {
+      for(let i = 0; i <= Object.keys(result.hnNews).length - 1; i++) {
         // For Hacker News cull any identifiers older than 1 day.
-        if ((moment() - moment(news[Object.keys(result.news)[0]]) ) / (24 * 60 * 60 * 1000) > 1) {
-          delete news[result.news[i]];
+        if ((moment() - moment(news[Object.keys(result.hnNews)[0]]) ) / (24 * 60 * 60 * 1000) > 1) {
+          delete news[result.hnNews[i]];
         }
       }
       
+      let hid = 0;
       for(let i = 0; i <= Object.keys(news).length - 1; i++) {
-        if (result.news[Object.keys(news)[i]]) {
+        let id = Object.keys(news)[i];
+        if (result.hnNews[id]) {
           // This news item is found have been seen more than five minutes ago.
-          if ((moment() - moment(news[Object.keys(result.news)[0]]) ) / (60 * 1000) > 0.1) {
-            $('#' + Object.keys(news)[i]).hide();
-            $('#' + Object.keys(news)[i]).next().hide();
+          if ((moment() - moment(result.hnNews[id]) ) / (60 * 1000) > 5) {
+            hid++;
+            $('#' + id).hide();
+            $('#' + id).next().hide();
           }
         } else {
           // New news item, so save it away.
-          result.news[Object.keys(news)[i]] = Object.values(news)[i];
+          result.hnNews[Object.keys(news)[i]] = Object.values(news)[i];
         }
       }
+      console.log('Hid ' + hid + ' stories that have been seen before.');
 
-      chrome.storage.local.set({'news': result.news}, function() {
-         console.log(Object.keys(news).length + ' entries saved to storage.');
+      chrome.storage.local.set({'hnNews': result.hnNews}, function() {
+         console.log(Object.keys(result.hnNews).length + ' entries saved to storage.');
+        });
+    });
+
+} else if (window.location.href === "https://old.reddit.com/") {
+
+  console.log("Did storage persist?");
+
+  chrome.storage.local.get(['redNews'], function(result) {
+      if (!result.redNews) {
+        result.redNews = {};
+      }
+
+      console.log('Read ' + Object.keys(result.redNews).length + ' news identifiers from storage.');
+
+      let things = $('.thing')
+      let now = Date.now();
+      let news = {};
+      things.each(function() { news[$(this).attr('id')] = now; });
+      console.log("Found " + Object.keys(news).length + " news identifiers in current page.");
+
+      for(let i = 0; i <= Object.keys(result.redNews).length - 1; i++) {
+        // For Hacker News cull any identifiers older than 1 day.
+        if ((moment() - moment(news[Object.keys(result.redNews)[0]]) ) / (24 * 60 * 60 * 1000) > 1) {
+          delete news[result.redNews[i]];
+        }
+      }
+      
+      let hid = 0;
+      for(let i = 0; i <= Object.keys(news).length - 1; i++) {
+        let id = Object.keys(news)[i];
+        if (result.redNews[id]) {
+          // This news item is found have been seen more than five minutes ago.
+          if ((moment() - moment(result.redNews[id]) ) / (60 * 1000) > 5) {
+            hid++;
+            $('#' + id).hide();
+            $('#' + id).next().hide();
+          }
+        } else {
+          // New news item, so save it away.
+          result.redNews[Object.keys(news)[i]] = Object.values(news)[i];
+        }
+      }
+      console.log('Hid ' + hid + ' stories that have been seen before.');
+
+      chrome.storage.local.set({'redNews': result.redNews}, function() {
+         console.log(Object.keys(result.redNews).length + ' entries saved to storage.');
         });
     });
 
